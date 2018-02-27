6f1f82e27b4aeca976ddacca30de5948e9573112
Ray Li <ray.li.verified@gmail.com>
2018-02-26 22:03:10 -0600

    Manifest Value Creation Part 2
    * Create Manifest Meta Value ReadMe.
    *Update Manifest Value to specify ad server instead of methods.

--- crosspromotion/src/main/java/stream/crosspromotion/Constants.java
+++ crosspromotion/src/main/java/stream/crosspromotion/Constants.java
@@ -6,5 +6,7 @@ public interface Constants {
 
     String CLIENT_ID = "1";  // Client ID | For identify the application | Example: 12567
 
+    String METHOD_ADS_GET = "ads.get.php";
+
     int AD_LIMIT = 20;
 }
